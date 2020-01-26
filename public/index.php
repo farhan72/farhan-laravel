f50537db194d6a095ac57513d005a687d45af57a
alfiy <alfi.cakap@gmail.com>
2020-01-26 20:56:23 +0700

    cors policy

--- public/index.php
+++ public/index.php
@@ -8,6 +8,9 @@
  */
 
 define('LARAVEL_START', microtime(true));
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, X-Requested-With");
 
 /*
 |--------------------------------------------------------------------------
